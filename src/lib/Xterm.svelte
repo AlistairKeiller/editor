7dba9562b990cded7c200a3a4ce0abbbc326d54f
Alistair Keiller <alistair@keiller.net>
2022-07-25 16:00:41 -0700

    add fit

--- src/lib/Xterm.svelte
+++ src/lib/Xterm.svelte
@@ -1,11 +1,15 @@
 <script>
     import 'xterm/css/xterm.css';
     import { Terminal } from 'xterm';
+    import { FitAddon } from 'xterm-addon-fit';
     import { afterUpdate } from 'svelte';
 
     afterUpdate(() => {
         const terminal = new Terminal();
+        const fitAddon = new FitAddon();
+        terminal.loadAddon(fitAddon);
         terminal.open(document.getElementById('terminal'));
+        fitAddon.fit();
         terminal.write('Hello from \x1B[1;3;31mxterm.js\x1B[0m $ ');
 	});
 </script>
