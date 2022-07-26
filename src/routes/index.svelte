e778d874545a65747ec29d232cbb77491e7db9ed
Alistair Keiller <alistair@keiller.net>
2022-07-25 22:22:43 -0700

    update bin with modularized and es6 clang

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,10 +1,12 @@
 <script>
-    import Codemirror from '../lib/Codemirror.svelte';
-    import Xterm from '../lib/Xterm.svelte';
+    // import Codemirror from '../lib/Codemirror.svelte';
+    // import Xterm from '../lib/Xterm.svelte';
+    import clang  from '../bin/clang++.js';
+    clang();
 </script>
 
-<Xterm/>
-<Codemirror/>
+<!-- <Xterm/>
+<Codemirror/> -->
 
 <style>
     :global(body) {
