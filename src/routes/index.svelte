094644786c2996e3acdb1b73e5744a04b3ed7fb6
Alistair Keiller <alistair@keiller.net>
2022-07-27 14:22:14 -0700

    update bin with new flags

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,9 +1,14 @@
 <script>
     // import Codemirror from '../lib/Codemirror.svelte';
     // import Xterm from '../lib/Xterm.svelte';
-    import clang  from '../bin/clang++.js';
-    clang();
+    import Clang from '../bin/clang++.js';
+    export const compile = async ( code ) => {
+        const clang = await Clang();
+        clang._main(["test", "test"]);
+    };
 </script>
+
+<button on:click={compile}>Compile</button>
 
 <!-- <Xterm/>
 <Codemirror/> -->
