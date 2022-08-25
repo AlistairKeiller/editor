f2e2dc51699ab7be2135118f0323e942ef7b544c
Alistair Keiller <alistair@keiller.net>
2022-08-25 08:16:41 -0700

    update bin

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,9 +2,17 @@
     // import Codemirror from '../lib/Codemirror.svelte';
     // import Xterm from '../lib/Xterm.svelte';
     import Clang from '../bin/clang++.js';
-    export const compile = async ( code ) => {
+    export const compile = async ( ) => {
         const clang = await Clang();
-        clang._main(["test", "test"]);
+        clang.FS.writeFile( 'main.cpp', 
+`#include <malloc.h>
+int main() {
+    return 0;
+}` );
+        // console.log(clang.FS.open('/'));
+        clang.callMain([]);
+        // clang.callMain([ '-cc1', 'main.cpp' ]);
+        // clang.callMain([ '-cc1', '-isysroot', '/lib/clang/wasi', '-internal-isystem', '/lib/clang/wasi/include', '-internal-isystem', '/lib/clang/16.0.0/include', 'main.cpp' ]);
     };
 </script>
 
