7234d6bfc39bf065264b1040bf375490a0f25798
Alistair Keiller <alistair@keiller.net>
2022-08-27 12:14:48 -0700

    mostly working

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,14 +5,16 @@
     export const compile = async ( ) => {
         const clang = await Clang();
         clang.FS.writeFile( 'main.cpp', 
-`#include <malloc.h>
+`#include <iostream>
 int main() {
     return 0;
 }` );
+        // clang._main();
         // console.log(clang.FS.open('/'));
-        clang.callMain([]);
+        // clang.callMain();
         // clang.callMain([ '-cc1', 'main.cpp' ]);
-        // clang.callMain([ '-cc1', '-isysroot', '/lib/clang/wasi', '-internal-isystem', '/lib/clang/wasi/include', '-internal-isystem', '/lib/clang/16.0.0/include', 'main.cpp' ]);
+        // clang.callMain([ '-cc1', '-I', '/lib/clang/wasi/include', '-I', '/lib/clang/wasi/include/c++/v1', '-I', '/lib/clang/16.0.0/include', 'main.cpp' ]);
+        clang.callMain([ '-cc1', '-emit-obj', '-disable-free', '-I', '/lib/clang/wasi/include', '-I', '/lib/clang/wasi/include/c++/v1', '-I', '/lib/clang/16.0.0/include', '-O2', '-o', 'test.o', '-x', 'c++', 'main.cpp' ]);
     };
 </script>
 
