5338b421e700092ae0bc10884de623311e333d82
Alistair Keiller <alistair@keiller.net>
2022-08-28 15:30:45 -0700

    add lld

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,20 +2,22 @@
     // import Codemirror from '../lib/Codemirror.svelte';
     // import Xterm from '../lib/Xterm.svelte';
     import Clang from '../bin/clang++.js';
+    import LD from '../bin/lld.js';
+
     export const compile = async ( ) => {
         const clang = await Clang();
+        const ld = await LD();
         clang.FS.writeFile( 'main.cpp', 
 `#include <iostream>
 int main() {
     return 0;
 }` );
-        // clang._main();
+        clang.callMain([ '-cc1', '-emit-obj', '-I', '/lib/clang/wasi/include', '-I', '/lib/clang/wasi/include/c++/v1', '-I', '/lib/clang/16.0.0/include', 'main.cpp' ]);
+        ld.FS.writeFile('main.o', clang.FS.readFile('main.o'));
+        ld.callMain([ '-flavor', 'wasm', '-o', 'main.wasm', 'main.o' ]);
+        console.log(ld.FS.open('/'));
         // console.log(clang.FS.open('/'));
-        // clang.callMain();
-        // clang.callMain([ '-cc1', 'main.cpp' ]);
-        // clang.callMain([ '-cc1', '-I', '/lib/clang/wasi/include', '-I', '/lib/clang/wasi/include/c++/v1', '-I', '/lib/clang/16.0.0/include', 'main.cpp' ]);
-        clang.callMain([ '-cc1', '-emit-obj', '-disable-free', '-I', '/lib/clang/wasi/include', '-I', '/lib/clang/wasi/include/c++/v1', '-I', '/lib/clang/16.0.0/include', '-O2', '-o', 'test.o', '-x', 'c++', 'main.cpp' ]);
-    };
+};
 </script>
 
 <button on:click={compile}>Compile</button>
