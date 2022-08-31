675c8f895c4699e160a4584ae4865ffbb019abe5
Alistair Keiller <alistair@keiller.net>
2022-08-30 18:39:49 -0700

    use embeded lib to lld

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,7 +2,7 @@
     // import Codemirror from '../lib/Codemirror.svelte';
     // import Xterm from '../lib/Xterm.svelte';
     import Clang from '../bin/clang++.js';
-    import LD from '../bin/lld.js';
+    import LD from '../bin/wasm-ld.js';
 
     export const compile = async ( ) => {
         const clang = await Clang();
@@ -12,10 +12,12 @@
 int main() {
     return 0;
 }` );
-        clang.callMain([ '-cc1', '-emit-obj', '-I', '/lib/clang/wasi/include', '-I', '/lib/clang/wasi/include/c++/v1', '-I', '/lib/clang/16.0.0/include', 'main.cpp' ]);
+        // console.log(clang.FS.open('/'));
+        console.log(clang.FS.open('/lib/clang/16.0.0/include'));
+        clang.callMain([ '-cc1', '-emit-obj', '-I', '/lib/clang/16.0.0/include/c++/v1', '-I', '/lib/clang/16.0.0/include', 'main.cpp' ]); // '-fcolor-diagnostics'
         ld.FS.writeFile('main.o', clang.FS.readFile('main.o'));
-        ld.callMain([ '-flavor', 'wasm', '-o', 'main.wasm', 'main.o' ]);
-        console.log(ld.FS.open('/'));
+        ld.callMain([ '-flavor', 'wasm', '-L/wlib', '-lc', '-lc++', '-lc++abi', '/wlib/libclang_rt.builtins-wasm32.a', '/wlib/crt1.o', 'main.o', '-o', 'main.wasm' ]);
+        // console.log(ld.FS.open('/'));
         // console.log(clang.FS.open('/'));
 };
 </script>
