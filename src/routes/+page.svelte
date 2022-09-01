852c4340e6359a904d0263c869b680a44d989a07
Alistair Keiller <alistair@keiller.net>
2022-08-31 17:28:18 -0700

    it works!

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -15,7 +15,7 @@ int main() {
         clang.callMain([ '-cc1', '-emit-obj', '-I', '/lib/clang/16.0.0/include/c++/v1', '-I', '/lib/clang/16.0.0/include', 'main.cpp' ]); // '-fcolor-diagnostics'
         ld.FS.writeFile('main.o', clang.FS.readFile('main.o'));
         ld.callMain([ '-flavor', 'wasm', '-L/wlib', '-lc', '-lc++', '-lc++abi', '/wlib/libclang_rt.builtins-wasm32.a', '/wlib/crt1.o', 'main.o', '-o', 'main.wasm' ]);
-        // console.log(ld.FS.open('/'));
+        console.log(ld.FS.open('/'));
         // console.log(clang.FS.open('/'));
 };
 </script>
