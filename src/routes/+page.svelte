f26a5bd3536afd299bf28a54549468e7bb6a4869
Alistair Keiller <alistair@keiller.net>
2022-09-15 18:38:27 -0700

    move bar to seperate file

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,5 @@
 <script>
+    import Bar from '$lib/Bar.svelte';
     import Codemirror from '$lib/Codemirror.svelte';
     import Xterm from '$lib/Xterm.svelte';
 
@@ -16,24 +17,10 @@
     //             console.log("error: Unkown data", e.data);
     //     }
     // };
-    let running = true;
-    function handleClick() {
-        running = !running;
-    }
 </script>
 
 <div class="grid h-screen grid-cols-2 grid-rows-[1fr_17fr] gap-1 bg-[#20232a]">
-    <div class="col-span-full flex justify-center mt-1">
-        {#if running}
-            <button on:click={handleClick} class="bg-green-800 text-green-300 hover:bg-green-600 hover:text-white font-bold w-32 rounded-xl transition-all outline-none drop-shadow-2xl">
-                Run
-            </button>
-        {:else}
-            <button on:click={handleClick} class="bg-red-800 text-red-300 hover:bg-red-600 hover:text-white font-bold w-32 rounded-xl transition-all outline-none drop-shadow-2xl">
-                Stop
-            </button>
-        {/if}
-    </div>
+    <Bar />
     <Codemirror />
     <Xterm />
 </div>
