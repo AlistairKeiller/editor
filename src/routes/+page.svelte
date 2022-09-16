86ee8c97def2b2988f7155317343c52a4b5cbd49
Alistair Keiller <alistair@keiller.net>
2022-09-15 17:16:39 -0700

    add temporary stop

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -16,11 +16,23 @@
     //             console.log("error: Unkown data", e.data);
     //     }
     // };
+    let running = true;
+    function handleClick() {
+        running = !running;
+    }
 </script>
 
 <div class="grid h-screen grid-cols-2 grid-rows-[1fr_17fr] gap-1 bg-[#20232a]">
     <div class="col-span-full flex justify-center mt-1">
-        <button class="bg-green-800 text-green-300 hover:bg-green-600 hover:text-white font-bold w-1/12 rounded-xl transition-all outline-none drop-shadow-2xl"> Run </button>
+        {#if running}
+            <button on:click={handleClick} class="bg-green-800 text-green-300 hover:bg-green-600 hover:text-white font-bold w-32 rounded-xl transition-all outline-none drop-shadow-2xl">
+                Run
+            </button>
+        {:else}
+            <button on:click={handleClick} class="bg-red-800 text-red-300 hover:bg-red-600 hover:text-white font-bold w-32 rounded-xl transition-all outline-none drop-shadow-2xl">
+                Stop
+            </button>
+        {/if}
     </div>
     <Codemirror />
     <Xterm />
